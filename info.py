d3452196b795c33d6766a6ca1a7b9ab00a19e7e8
parsnips1966 <106930780+parsnips1966@users.noreply.github.com>
2022-06-25 23:34:34 +0100

    tried to use json to manage data

--- info.py
+++
@@ -1,5 +0,0 @@
-details = {
-    "parsonsjm20@haybridge.worcs.sch.uk": "password",
-    "hackettdj20@haybridge.worcs.sch.uk": "josephparsons"
-}
-countries = ["Senegal", "Netherlands", "Qatar", "Ecuador"]
--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from flask import Flask, render_template, request
 import json
-import info
 
-details = info.details
-countries = info.countries
+with open("info.json", "r") as file:
+    info = json.load(file)
 
 app = Flask(__name__)
 app.debug = True
@@ -22,35 +21,37 @@ def signup():
 
 @app.route("/formnew", methods=['GET', 'POST'])
 def formnew():
-    global username
+    global email, username
     email = request.form['email']
     username = email[0: email.index('@')]
-    password = request.form['password']
-    if email not in details.keys():
-        details[email] = password
-        save = json.dumps(details)
-        with open("details.json", "w") as file:
-            file.write(save)
-        return render_template("form.html", username=username, countries=countries)
-    return render_template("home.html", error="That email is already in use, please login or use another one.")
+    input_password = request.form['password']
+    #if email not in details.keys():
+    #    details[email].password = input_password
+    #    save = json.dumps(details)
+    #    with open("details.json", "w") as file:
+    #        file.write(save)
+    return render_template("form.html", username=username)
+    #return render_template("home.html", error="That email is already in use, please login or use another one.")
 
 @app.route("/formreturn", methods=['GET', 'POST'])
 def formreturn():
+    global email, username
     email = request.form['email']
     username = email[0: email.index('@')]
-    password = request.form['password']
-    if email in details.keys():
-        if password == details[email]:
-            return render_template("form.html", username=username, countries=countries)
-        return render_template("home.html", error="That password is incorrect.")
-    return render_template("home.html", error="That email is not registered.")
+    input_password = request.form['password']
+    #if email in info():
+    #    if input_password == info[email].password:
+    return render_template("form.html", username=username)
+    #    return render_template("home.html", error="That password is incorrect.")
+    #return render_template("home.html", error="That email is not registered.")
 
 @app.route("/submitted", methods=['GET', 'POST'])
 def submitted():
-    items = []
-    for i in request.form:
-        items.append(request.form[i])
-    return render_template("submitted.html", username=username, items=items)
+    #for i in request.form:
+    #    info[email].predictions.insert(i, request.form[i])
+    #print(info[email].predictions)
+    return render_template("submitted.html", username=username)
+    #, predictions=info[email].predictions goes on previous line
 
 if __name__ == "__main__":
     app.run()
